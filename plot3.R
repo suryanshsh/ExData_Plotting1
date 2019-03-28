31d5a1dec6f1ff8acc7d53515a484d004e7387fe
SURYANSH SHRIVASTAVA <39276431+suryanshsh@users.noreply.github.com>
2019-03-28 16:49:47 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -4,7 +4,7 @@ hcp <- hcp[hcp$Date %in% c("1/2/2007","2/2/2007") ,]
 hcp$Date <- as.Date(hcp$Date,format="%d/%m/%Y")
 #hcp$Date <- weekdays(hcp$Date)
 x_axis <- strptime(paste(hcp$Date,hcp$Time,sep=" "),"%Y-%m-%d %H:%M:%S")
-png("plot3.png",width =480 ,height = 480)
+png("plot3.png",width =500 ,height = 500)
 plot(x_axis,hcp$Sub_metering_1,type="l",ylab="Energy submetering",xlab = "")
 lines(x_axis,hcp$Sub_metering_2,col="red",type = "l")
 lines(x_axis,hcp$Sub_metering_3,col="blue",type = "l")

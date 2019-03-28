3a504fa6774c02d8d26249bf9de9f5088e814b8b
SURYANSH SHRIVASTAVA <39276431+suryanshsh@users.noreply.github.com>
2019-03-28 16:48:21 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,6 +2,6 @@ addrs <- "household_power_consumption.txt"
 hcp <- read.table(addrs, sep=";", stringsAsFactors=FALSE, dec=".", header=T)
 hcp <- hcp[hcp$Date %in% c("1/2/2007","2/2/2007") ,]
 hcp$Date <- as.Date(hcp$Date,format="%d/%m/%Y")
-png("plot1.png",width =480 ,height = 480)
+png("plot1.png",width =500 ,height = 500)
 hist(as.numeric(hcp$Global_active_power) , col='red',xlab="Global Active Power (killowatts)",main="Global Active Power")
 dev.off()

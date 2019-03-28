526b3c66f0a0e38243c641e3aea761bd3dd04c37
SURYANSH SHRIVASTAVA <39276431+suryanshsh@users.noreply.github.com>
2019-03-28 16:48:49 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,6 +4,6 @@ hcp <- hcp[hcp$Date %in% c("1/2/2007","2/2/2007") ,]
 hcp$Date <- as.Date(hcp$Date,format="%d/%m/%Y")
 #hcp$Date <- weekdays(hcp$Date)
 x_axis <- strptime(paste(hcp$Date,hcp$Time,sep=" "),"%Y-%m-%d %H:%M:%S")
-png("plot2.png",width =480 ,height = 480)
+png("plot2.png",width =500 ,height = 500)
 plot(x_axis,hcp$Global_active_power,type="l",xlab = "",ylab ="Global Active Power (killowatts)" )
 dev.off()
